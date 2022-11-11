957cc033cd5d791f7ebd385ae669711c0faacc5e
Mike Job <mikejob@Mikes-MacBook-Air.local>
2022-11-10 23:09:53 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Bateria Geropsicomotora',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
